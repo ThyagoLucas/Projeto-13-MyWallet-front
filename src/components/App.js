5745709cf41e5680e9c9101b1ee718c1726865e5
Thyago Lucas <Salc@icomp.ufam.edu.br>
2022-05-08 03:33:10 -0400

    funções de entrada e saidas concluidas no front-end, junto com a redenrização das transações na tela home

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import CashIn from "./CashIn";
+import CashOut from "./CashOut";
 import Home from "./Home";
 import Login from "./Login";
 import Register from "./Register";
@@ -11,6 +13,8 @@ function App(){
                 <Route path="/" element={<Login/>}></Route>
                 <Route path="/cadastro" element={<Register/>}></Route>
                 <Route path="/home" element={<Home/>}></Route>
+                <Route path="/entrada" element={<CashIn/>}></Route>
+                <Route path="/saida" element={<CashOut/>}></Route>
             </Routes>
         </BrowserRouter>
     )
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,60 +2,91 @@ import styled from "styled-components";
 import {GrLogout, GrAddCircle, GrSubtractCircle} from 'react-icons/gr';
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 
 function Home(){
 
     const token = localStorage.getItem('token');
 
-    const [userDatas, setUserDatas] = useState({name:'', values:[], total:''});
-
+    const [userDatas, setUserDatas] = useState({name:'', transactions:[], total:0});
+   
     useEffect(()=>{
-  
         const config = {
             headers: {
                 "Authorization": `Bearer ${token}`
             }
         }
-    
-
         axios.get('http://localhost:5000/info-account',config)
-                .then((response)=>{
-                    setUserDatas({...userDatas, name:response.data})
-                   })
+            .then((response)=>{
+                setUserDatas(response.data) })
 
-                .catch(err => {
-                    console.log('Erro ao buscar dados da api: ', err);
-                });  
+            .catch(err => {
+                console.log('Erro ao buscar dados da api: ', err);
+            });  
     },[])
 
     return(
 
         <Main>
             <header>
-                <h1>Ola {userDatas.name} </h1>
+                <h1>Ola {userDatas.name},</h1>
                 <GrLogout color="white"/>
             </header>
 
             <section>
-                <h1>
-                    Não há registros de entrada ou de saída!
-                </h1>
+                {userDatas.transactions.length === 0 
+                ?   <h1>Não há registros de entrada ou de saída!</h1>
+
+                :  
+                <>
+                    <div className="transactions">
+                        {userDatas.transactions.map((value, index) => <Values key={index} infoTransition={value} />)}
+
+                    </div>
+                    <h1 className="total">Saldo: <span className={`${userDatas.total > 0? 'cashIn':'cashOut'}`}>{userDatas.total.toFixed(2)}</span></h1>
+
+                </>         
+            }
+                
             </section>
 
             <footer>
-                <div>
-                    <GrAddCircle/>
-                    <p>Nova entrada</p>
-                </div>
 
-                <div>
-                    <GrSubtractCircle/>
-                    <p>Nova saída</p>
-                </div>
-
+                <Link to={"/entrada"}>
+                    <div>
+                        <GrAddCircle/>
+                        <p>Nova entrada</p>
+                    </div>
+                </Link>
+                
+                <Link to={"/saida"}>
+                    <div>
+                        <GrSubtractCircle/>
+                        <p>Nova saída</p>
+                    </div>
+                </Link>
+                
             </footer>
         </Main>
+    )
+}
+
+function Values ({infoTransition}){
+
+    const {day, month, typeOperation, description, value} = infoTransition;
+    const newValue = value.toFixed(2).toString().replace('.',',');
+    console.log('infoTransitions: ', infoTransition);
+
+    return(
+        <div className="transaction">
+            <span>
+                <h4 className="data">{day}/{month}</h4>
+                <p className="description">{description}</p>
+            </span>
+            
+            {typeOperation === 'cashIn' ? <h4 className="cashIn">{newValue}</h4>:<h4 className="cashOut">{newValue}</h4>}
+        </div>  
     )
 }
 
@@ -85,13 +116,64 @@ const Main = styled.main`
         font-size: 20px;
         line-height: 23px;
         display: flex;
+        flex-direction: column;
         align-items: center;
-        text-align: center;
+        justify-content: space-between;
         color: #868686;
         margin-top: 20px;
         height: 75%;
         width: 100%;
         background-color: #FFFFFF;
+        border-radius: 5px;
+    }
+    section h1{
+        width: 65%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+    }
+    .transactions{
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    .transaction{
+        font-size: 18px;
+        width: 90%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        margin-top: 3px;
+    }
+    .transaction span{
+       
+        width: 75%;
+        display: flex;
+        flex-direction: row;
+    }
+    .transaction span p{
+        margin-left: 10px;
+    }
+    .cashIn{
+        color: green;
+    }
+    .cashOut{
+        color:red;
+    }
+    .description{  
+     
+        color: #000000;
+    }
+    .total{
+        background-color: lightblue;
+        width: 90%;
+        height: 35px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        color: #000000;
     }
     footer{
         width: 100%;
